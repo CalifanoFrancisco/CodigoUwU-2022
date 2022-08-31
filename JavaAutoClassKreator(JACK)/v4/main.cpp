973558107151d0345a9dc5a5a2f966c17d03f738
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2022-08-31 11:49:42 -0300

    Update main.cpp

--- JavaAutoClassKreator(JACK)/v4/main.cpp
+++ JavaAutoClassKreator(JACK)/v4/main.cpp
@@ -102,11 +102,11 @@ class Class {
     //Operator help :P
         /*bool operator==(Class x){
             return 
-                (this->name        == x.name       ) ? (
-                (this->inheritance == x.inheritance) ? (
-                (this->variables   == x.variables  ) ? (
-                (this->imports     == x.imports    ) ? (
-                (this->methods     == x.methods    ) ?
+                (this->name                == x.name                 ) ? (
+                (this->inheritance         == x.inheritance          ) ? (
+                (this->variables.getData() == x.variables.getData()  ) ? (
+                (this->imports.getData()   == x.imports.getData()    ) ? (
+                (this->methods.getData()   == x.methods.getData()    ) ?
                 true:false):false):false):false):false
             ;
         }*/
@@ -117,31 +117,124 @@ class File {
 private: 
     std::string name = "";
     std::string extension = ".txt";
+    std::string fileName = "";
+
+    bool isOpenable(){
+        if((this->name != "" && this->extension != "") || this->fileName != ""){
+            return true;
+        }
+        println(std::string(this->fileName + " cannot be opened"));
+        return false;
+    }
+    bool exists(){
+        std::fstream file(this->fileName);
+        if(file){
+            println(std::string(fileName + " already exists."));
+            return true;
+        }
+        return false;
+    }
+    void setFileName (){
+        this->fileName = this->name + this->extension;
+    }
 public:
-//--------------------- WIP ---------------------//
-
+//--------------------- TODO ---------------------//
+//Setters
+    void setName     (std::string x){ 
+        this->name = x; 
+        this->fileName = this->name + this->extension;
+    }
+    void setExtension(std::string x){
+        this->extension = x;
+        this->fileName = this->name + this->extension;
+    }
+    void setFileName (std::string x){
+        this->fileName = x;
+    }
+//Builders
     bool buildNewFile(){
+        //Checks
+        if(!isOpenable()) return false;
+        if(exists())      return false;
 
-        if(this->name == "") println("File has no name"); return false;
+        std::ofstream file(this->fileName);
 
-        std::string totalName = this->name + this->extension;
-
-        std::ifstream file(totalName);
-
-        if(file) println(std::string(totalName + " already exists.")); return false;
-        
+        file.close();
+        //Succesful creation
+        println(std::string(this->fileName + " created succesfully"));
         return true;
     }
-    
     void buildClass();
+//Readers
+    std::string read(){
+        std::ifstream file(this->fileName);
+        std::string word = "";
+        while (file >> word){
+            // displaying content
+            std::cout << word << std::endl;
+        }
+        return "";
+    }
+    void readFromUntil(std::string a,std::string b){
+        std::ifstream file(this->fileName);
+        std::string word;
+        bool sno = false;
+        while(file >> word){
+            if(word == a) sno = true;
+            if(sno)       println(word);
+            if(word == b) sno = false;
+        }
+    }
+    void readBetween(std::string a,std::string b){ //NO ANDA !!! >:(
+        std::ifstream file(this->fileName);
+        std::string word;
+        bool sno = false;
+        while(file >> word){
+            if(word == a) file >> word; sno = true;
+            if(sno)       println(word);
+            if(word == b) sno = false;
+        }
+    }
+//Writers
+    void write(std::string x){
+        std::ofstream file(this->fileName);
+        file << x << std::endl;
+    }
+    void write(std::vector<std::string> x){
+        std::ofstream file(this->fileName);
+        for (int i = 0; i < x.size(); i++){
+            file << x[i] << std::endl;
+        }
+    }
+//Adders for external help files 
     void addDataStructure();
     void addVariableType();
- 
+//Class wirter
+    bool writeClass(Class c){
+    //Building file
+        this->fileName  = c.getName();
+        this->extension = ".java";
+        this->setFileName();
+        std::ofstream file(this->fileName);
+    //Builder
+        //Imports
+        for (int i = 0; i < c.getImports().size(); i++) file << c.getImports()[i] << ";" << std::endl;
+        file << std::endl;
+        //Main structure
+        file << "public class" << c.getName();
+        if(c.isInheritance()) file << " extends " << c.getInheritance() << " {" << std::endl;
+        
+    //Success
+        file.close();
+        println("Class built succesfully");
+        return true;
+    };
 };
 
 int main(){
-    HashSet<Class> clases;
-
-
+    File file;
+    file.setFileName("dataTypes.txt");
+    file.readFromUntil("Generic{","}");
+    //file.readBetween("Primitive{","}");
     return 0;
 }
