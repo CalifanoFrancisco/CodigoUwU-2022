3db98b2a3ef63b36b42d903239101ec10139db21
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2022-09-05 11:41:22 -0300

    Update HashSet.cpp

--- Kali's C++ libraries/HashSet/HashSet.cpp
+++ Kali's C++ libraries/HashSet/HashSet.cpp
@@ -1,46 +1,87 @@
 #include <iostream>
-#include <vector>
-
 template <typename T> class HashSet {
-    private:
-        std::vector<T> data;
-    public:
-        //Constructors
-        HashSet(){/*@*/};
-        HashSet(std::vector<T> x) {
-            for (int i = 0; i < x.size(); i++) this->data.add(x[i]);
+private:
+    T         dato;
+    HashSet<T>*next;
+    HashSet<T>*head = NULL;
+    bool isHere(T e){
+        HashSet<T>*current = head;
+        while (current != NULL) {
+            if (current->dato == e) return true;
+            current = current->next;
         }
-  
-        //Methods
-        void add   (T x) {
-            if (!isHere(x)) this->data.push_back(x);
+        return false;
+    }
+    bool inBounds(int i){
+        if (i < 0 || i > this->size()) return false;
+        return true;
+    }
+public:
+    void add(T x){
+        if(isHere(x)) return;
+
+        HashSet<T>*newNode = new HashSet<T>;
+        newNode->dato = x;
+        newNode->next = NULL;
+
+        if(head == NULL){
+            head = newNode;
+            return;
         }
-        void remove(int i) {
-            if(i <= this->data.size()) this->data.erase(this->data.begin()+i);
-        }
-        T    get   (int i) {
-            if (i <= this->data.size()) return this->data[i];
-        }
-        int  find  (T x) {
-            if (!isHere(x)) return NULL;
-            for (int i = 0; i < this->data.size(); i++){
-                if (this->data[i] == x) return i;
+
+        HashSet<T>*current = head;
+        while (current != NULL) {
+            if (current->next == NULL) {
+                current->next = newNode;
+                return;
             }
+            current = current->next;
         }
-        bool isHere(T x) {
-            for (int i = 0; i < this->data.size(); i++){
-                if (this->data[i] == x) return true;
+    }
+    T    operator[](int index){
+        if (index > size() || index < 0) return T();
+        HashSet<T>*current = head;
+        for(int i = 0;i < index; i++) current = current->next;
+        return current->dato;
+    }
+    bool operator==(HashSet e){
+        
+        if (!inBounds(e.size())) return false;
+
+        HashSet<T>*current = head;
+        for (int i = 0; current != NULL; i++){
+            if (current->dato != e[i]) return false;
+            current = current->next;
+        }
+        return true;
+    }
+    int  size(){
+        HashSet<T>*current = head;
+        int i = 0;
+        for (; current != NULL; i++) current = current->next;
+        return i;
+    }
+    void println(){
+        HashSet<T>*current = head;
+        for(; current!=NULL; current = current->next){
+            std::cout << current->dato << " ";
+        }
+        std::cout << std::endl;
+    }
+    void remove(int index){
+
+        if (!inBounds(index)) return;
+
+        HashSet<T>*current = head;
+        HashSet<T>*before  = NULL;
+        for(int i = 0; current != NULL; i++){
+            if (index == i){
+                before->next = current->next;
+                delete current;
+                return;
             }
-            return false;
+            before  = current;
+            current = current->next;
         }
-        void sayAll() {
-            for (int i = 0; i < this->data.size();i++) std::cout << this->data[i] << " ";
-            std::cout << std::endl;
-        }
-        int  size  () {
-            return this->data.size();
-        };
-        std::vector<T> getData(){
-            return this->data;
-        };
+    } 
 };
