9cbc09f7aeceb26ab7b51b331ae2e4cdb58ef11c
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2022-09-13 20:59:09 -0300

    Update main.cpp

--- JavaAutoClassKreator(JACK)/v6/main.cpp
+++ JavaAutoClassKreator(JACK)/v6/main.cpp
@@ -5,12 +5,18 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
 
 #define DEBUG 1
 #if DEBUG
-    #define println(x) std::cout<<x<<std::endl;
+    #define println(x) std::cout << x << std::endl
+    #define print(x)   std::cout << x 
 #else
     #define println(x)
+    #define print(x)
 #endif
 
 
@@ -96,6 +102,13 @@ public:
 
     bool operator== (Variable x) {
         return (this->name == x.name);
+    }
+    Variable(std::string _type = "",std::string _name = "",std::string _def = "")
+    :type(_type), name(_name), def(_def)
+    {}
+
+    std::string toString(){
+        return this->type + " " + this->name + " = " + this->def + ";";
     }
 };
 
@@ -192,6 +205,20 @@ public:
     bool hasInterface(){
         return !(this->interfaces.size() == 0);
     }
+    HashSet<Variable> getAllAttributes(){
+        HashSet<Variable> output;
+        for (int i = 0; i < this->attributes.size(); i++) output.add(this->attributes[i]);
+        if (this->hasInheritance()){
+            bool inheritanceLoop = this->hasInheritance();
+            Class auxClass = *(this->getInheritance());
+            while (inheritanceLoop) {
+                for (int i = 0; i < auxClass.getAttributes().size(); i++) output.add(auxClass.getAttributes()[i]);
+                inheritanceLoop = auxClass.hasInheritance();
+                if (inheritanceLoop) auxClass = *(auxClass.getInheritance());
+            }
+        }
+        return output;
+    }
 
     bool operator==(Class x){
         return (x.name == this->name);
@@ -212,6 +239,15 @@ public:
         return (this->name == x.name);
     }
 };
+
+std::string readUntil(std::string input,char until){
+    std::string output = "";
+    for (int i = 0; i < input.size(); i++) {
+        if (input[i] == until) break;
+        output += input[i];
+    }
+    return output + ')';
+}
 
 std::string readUntilParenthesis(std::string input){
     std::string output;
@@ -256,7 +292,7 @@ class JavaFile {
         file << std::endl;
         //Methods
         for (int i = 0; i < x.getMethods().size(); i++){
-            file << space << readUntilParenthesis(x.getMethods()[i]) << ";" << std::endl;
+            file << space << readUntil(x.getMethods()[i],')') << ";" << std::endl;
         }
         file << "}" << std::endl;
         file.close();
@@ -281,6 +317,9 @@ class JavaFile {
 };
 
 class HelpFile {
+    // CLASS     $
+    // INTERFACE &
+    // ENUM      %
 private:
     std::string name;
     std::vector<std::string> copyFileIntoBuffer () {
@@ -306,16 +345,78 @@ private:
             file << buffer[i] << std::endl;
         }
     }
+    bool checkFileHas(std::string input){
+        std::ifstream file(this->name);
+        std::string word;
+        if (file){
+            while (file >> word) {
+                if (word == input) return true;
+            }
+            return false;
+        }
+        return false;
+    }
+    bool exists(){
+        std::fstream file(this->name);
+        if(file){
+            println(std::string(this->name + " already exists."));
+            file.close();
+            return true;
+        }
+        file.close();
+        return false;
+    }
+    bool isEmpty(){
+        std::fstream file(this->name);
+        std::string word;
+        int i = 0;
+        for (; file >> word; i++){}
+        bool empty = (i == 0);
+        if (empty) println("HelpFile.txt is empty :(");
+        return empty;
+    }
+    void build(){
+        std::ofstream file(this->name);
+        file.close();
+    }
+    bool isAlreadyInFile(Class x) {     //WIP 
+        std::ifstream file(this->name);
+        std::string word;
+        std::vector<std::string> buffer;
+        bool out = false;
+        while (file >> word) buffer.push_back(word);
+        for (int i = 0; i < buffer.size(); i++){
+            cout << "Is " << buffer[i][0] <<" == $ ?   ";
+            if (buffer[i][0] == '$'){
+                int c = 0;
+                cout<<"SI"<<endl;
+                std::string aux = buffer[i];
+                cout << "Palabra a comparar " << aux << endl;
+                for (int j = 0; j < x.name.size(); i++){ //Se queda encerrado adentro de D == A ? NO
+                    cout << "Es " << aux[j+1] << " == " << x.name[j] << " ?";
+                    if (aux[j+1] == x.name[j]){
+                        c++;
+                        cout<<"Si"<<endl;
+                    }else{
+                        cout<<"no"<<endl;
+                    }
+                }
+                if (c == x.name.size()) out = true;
+            }else{
+                cout <<"no"<<endl;
+            }
+        }
+        return false;
+    }
 public:
     HelpFile(){
         this->name = "HelpFile.txt";
+        if (!this->exists()) this->build();
+        if (!this->isEmpty()) return;
         std::ofstream file(this->name);
-        file << "#INTERFACE"     << std::endl;
-        file << "#END_INTERFACE" << std::endl;
-        file << "#ENUM"          << std::endl;
-        file << "#END_ENUM"      << std::endl;
-        file << "#CLASS"         << std::endl;
-        file << "#END_CLASS"     << std::endl;
+        file << "@INTERFACE" << std::endl;
+        file << "@CLASS"     << std::endl;
+        file << "@ENUM"      << std::endl;
         file.close();
     }
 
@@ -323,26 +424,72 @@ public:
         std::fstream file(this->name);
         std::vector<std::string> buffer = this->copyFileIntoBuffer();
         std::string interface = "";
-        interface += x.name + "{";
-        for (int i = 0; i < x.getMethods().size(); i++) interface += x.getMethods()[i] + ";"; 
-        interface += "} #END_" + x.name;
-        writeIntoBuffer(buffer, "#INTERFACE", interface);
+        interface += "&" + x.name + "{";
+        for (int i = 0; i < x.getMethods().size(); i++) interface += readUntilParenthesis(x.getMethods()[i]) + ";";
+        interface += "}";
+        writeIntoBuffer(buffer, "@INTERFACE", interface);
         writeBufferIntoFile(buffer);
 
     }
-    void addIntoHelpFile (Class     x) {}
-    void addIntoHelpFile (Enum      x) {}
+    void addIntoHelpFile (Class     x) {
+        std::fstream file(this->name);
+        std::vector<std::string> buffer = this->copyFileIntoBuffer();
+        std::string class_ = "";
+        class_ += "$" + x.name + "(";
+        for (int i = 0; i < x.getAllAttributes().size(); i++){
+            class_ += x.getAllAttributes()[i].type + "-" + x.getAllAttributes()[i].name;
+            if (i < x.getAllAttributes().size()-1) class_ += ",";
+        }
+        class_ += "){}";
+        writeIntoBuffer(buffer, "@CLASS", class_);
+        writeBufferIntoFile(buffer);
+    }
+    void addIntoHelpFile (Enum      x) {
+        //Symbol %
+    }
+    bool isInFile(Class x){
+        return this->isAlreadyInFile(x);
+    }
 };
 
 //Interfaces work correctly :D
+//Instead of using #TYPE #END_TYPE use a character for each type
 
 int main () {
-    HelpFile coso = HelpFile();
+    /*HelpFile coso = HelpFile();
     Interface x;
     x.name = "COso";
     x.addMethod("public void coso(){ lol teo }");
     x.addMethod("public int  getChota(){return this.Chota}");
-    coso.addIntoHelpFile(x);
+    coso.addIntoHelpFile(x);*/
+    /*int num = 10;
+    int*ptr = &num;
+    cout << ptr  << endl;
+    cout << *ptr << endl;*/
+
+    Class a,b,c,d;
+    a.name = "A"; b.name = "B";
+    c.name = "C"; d.name = "D";
+    a.addAttribute(Variable("Integer","edad"  ,"0"          ));
+    b.addAttribute(Variable("String" ,"nombre","\"ricardo\""));
+    c.addAttribute(Variable("float"  ,"num"   ,"20"         ));
+    d.addAttribute(Variable("double" ,"num2"  ,"78"         ));
+    c.setInheritance(&d);
+    b.setInheritance(&c);
+    a.setInheritance(&b);
+    Interface x;
+    x.name = "Coso";
+    x.addMethod("public void coso(){ lol teo }");
+    x.addMethod("public int  getChota(){return this.Chota}");
+
+    HelpFile file = HelpFile();
+
+    //file.addIntoHelpFile(a);
+    //file.addIntoHelpFile(b);
+    //file.addIntoHelpFile(c);
+    //file.addIntoHelpFile(d);
+    //file.addIntoHelpFile(x);
+    cout << file.isInFile(a) << endl;
 
     cout<<"Success!"<<endl;
     return 0;
